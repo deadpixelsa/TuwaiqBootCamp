fedabc918325a13a8c85c18dfe2f35adc01c8aff
deadpixelsa <31051116+deadpixelsa@users.noreply.github.com>
2023-01-01 18:54:22 +0300

    Add files via upload

---
+++ ExOne.kt
@@ -0,0 +1,20 @@
+package com.example.tuwaiqone
+
+fun main() {
+
+    var x:String ="Abdullah"
+    var y:Float = 20.6F
+    var z:Double = 8.9746325
+    var t:Byte = 25
+    var r:Short = 2022
+    var s:Long = 321984526954
+    var g:Boolean = true
+    var p: Char ='a'
+
+    println(x+y+z+t+r+s+g+p)
+
+
+
+
+
+}
