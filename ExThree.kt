d1a046542aeacac326c57c148978f19ac6ae6a75
deadpixelsa <31051116+deadpixelsa@users.noreply.github.com>
2023-01-02 22:44:22 +0300

    Add files via upload

---
+++ ExThree.kt
@@ -0,0 +1,17 @@
+package com.example.myapplicationtwo
+
+fun main(){
+
+   for (i in 0..100) {
+       if (i >= 71){
+          println("IT'S OVER 70")
+
+
+
+      }else println(i)
+
+
+   }
+
+
+}
