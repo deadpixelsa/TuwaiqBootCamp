6641fcd8f573cee7061d91730b5bd111f7f7f8df
deadpixelsa <31051116+deadpixelsa@users.noreply.github.com>
2023-01-09 12:14:46 +0300

    Update FirstProject.kt

--- FirstProject.kt
+++ FirstProject.kt
@@ -1,39 +1,48 @@
-package com.example.myapplication12
+package com.example.firsthomework
+
 fun main(){
-val task1= Task(1,"mohammed","First Grade",true)
-val task2= Task(2,"khalid","Second Grade",true)
-Todo().addTask(task = task1)
-Todo().addTask(task = task2)
+
+    val todo = Todo()
+    val task1= Task(1,"mohammed","First Grade",true)
+    val task2= Task(2,"khalid","Second Grade",true)
+    todo.addTask(task = task1)
+    todo.addTask(task = task2)
+    todo.specificTask(task2.id)
+//todo.show()
 
 
 
 }
 
 
- class Todo(){
+ open class Todo{
+
+     fun specificTask(id: Int){
+
+         println(id)
+
+     }
 
    private val tasks = arrayListOf<Task>()
 
     fun show(){
-if (tasks.isNotEmpty()){
         for (i in tasks){
             println("$tasks")
-            return
+
         }
-    }else
-        println("array is empty")
     }
-      fun addTask(task: Task) {
+    fun addTask(task: Task) {
         tasks.add(task)
-        println("Item added")
-          println(task)
+        tasks.add(task)
+       // println(tasks)
         return
 
 
     }
     fun removeTask(task: Task){
         tasks.remove(task)
-        println("records has been Deleted")
+        tasks.remove(task)
+        println("records Deleted")
         return
 
 
@@ -43,8 +52,7 @@ if (tasks.isNotEmpty()){
             task.isCompleted = true
             println("records Changed")
             return
-        }else
-            println("No need to Change record it's true")
+        }
 
 
     }
@@ -54,3 +62,4 @@ if (tasks.isNotEmpty()){
 data class Task(val id:Int = 0, val name:String ="",val note:String ="",var isCompleted:Boolean = false){
 
 }
+
